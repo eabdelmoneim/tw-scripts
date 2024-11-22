90614cfd3b5a19e355affacef0701a31324c4744
Eiman <eabdelmoneim@gmail.com>
2024-11-21 20:31:43 -0600

    added encode and send

--- sig-mint.ts
+++ sig-mint.ts
@@ -1,4 +1,4 @@
-import { createThirdwebClient, getContract, sendTransaction } from "thirdweb";
+import { createThirdwebClient, encode, getContract, prepareTransaction, sendTransaction } from "thirdweb";
 import { privateKeyToAccount } from "thirdweb/wallets";
 import { defineChain } from "thirdweb/chains";
 import { generateMintSignature, mintWithSignature } from "thirdweb/extensions/erc721";
@@ -12,10 +12,15 @@ async function main() {
     secretKey: process.env.THIRDWEB_SECRET_KEY as string,
   });
 
+  //////////////////////////////////////////////////////////////
+  // SERVER SIDE CODE STARTS HERE
+  //////////////////////////////////////////////////////////////
+
   // Initialize an account from the private key
+  // PRIVATE KEY2 HAS MINTER ROLE ON CONTRACT
   const account = privateKeyToAccount({
     client,
-    privateKey: process.env.EOA_PRIVATE_KEY!,
+    privateKey: process.env.EOA_PRIVATE_KEY2!,
   });
 
   // Define the chain using environment variable
@@ -24,7 +29,7 @@ async function main() {
   // Get the contract
   const contract = getContract({
     client,
-    address: "0x507fb266c2CaBf31c78636dF9D427a29C76805DC",
+    address: "0xdD98DdE731e7Ff82101Edb767822BaAfC5a9C015",
     chain,
   });
 
@@ -53,9 +58,30 @@ async function main() {
     signature,
   });
 
+  const encodedTx = await encode(transaction);
+
+  //////////////////////////////////////////////////////////////
+  // CLIENT SIDE CODE STARTS HERE
+  //////////////////////////////////////////////////////////////  
+
+  // Initialize an account from the private key
+  // PRIVATE KEY1 HAS NO ROLE ON CONTRACT and is the WALLET that will be sending the transaction
+  const account2 = privateKeyToAccount({
+    client,
+    privateKey: process.env.EOA_PRIVATE_KEY!,
+  });
+
+
+  const rawTx = prepareTransaction({
+    to: "0xdD98DdE731e7Ff82101Edb767822BaAfC5a9C015",
+    chain,
+    client,
+    data: encodedTx
+  });
+
   const result = await sendTransaction({
-    transaction,
-    account,
+    transaction: rawTx,
+    account: account2,
   });
 
   console.log("Transaction hash:", result.transactionHash);
